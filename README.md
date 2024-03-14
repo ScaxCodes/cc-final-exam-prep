25eae409f85a60bcb82aa443766fb23880594410
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-03-14 21:57:57 +0100

    Solve first question

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cc-final-exam-prep
+
 Some practise questions for my final exam of the course "CourseCareers - Software Development Fundamentals"
+
+## Question 1
+
+Write a function that accepts an array of numbers and returns the 2 largest numbers in the array. The largest values should be returned in an array where the first element in the array is the larger of the two elements. You may assume you will always be given an array that contains at least 2 numbers.
---
+++ question-1/script.js
@@ -0,0 +1,33 @@
+function getTwoLargestInt(array) {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (number of array) {
+    if (number >= largest) {
+      secondLargest = largest;
+      largest = number;
+    } else if (number < largest && number > secondLargest)
+      secondLargest = number;
+  }
+
+  return [largest, secondLargest];
+}
+
+console.log(getTwoLargestInt([7, -2, 3, 4, 5, 1, 23, 31, 23]));
+console.log(getTwoLargestInt([2, 1]));
+console.log(getTwoLargestInt([100, 200, 200, 100]));
+console.log(getTwoLargestInt([-1, -2, -3, -4, -5]));
+console.log(getTwoLargestInt([5, 7, 9, 1, 3]));
+console.log(getTwoLargestInt([0, 0, 0, 0, 0]));
+console.log(
+  getTwoLargestInt([
+    5, 7, 9, 1, 3, 11, 15, 12, 18, 23, 44, 32, 49, 50, 31, 60, 70, 80, 90, 100,
+    101, 102, 200, 500, 250,
+  ])
+);
+console.log(
+  getTwoLargestInt([
+    100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400,
+    1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500,
+  ])
+);
