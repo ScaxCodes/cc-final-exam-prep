f81dfb26c89e5c87301075f262023a591d6433f6
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-03-21 07:36:10 +0100

    Add solution for go question 1

--- question-1-go/main.go
+++ question-1-go/main.go
@@ -1,13 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func getTwoLargestInteger(numbers []int) []int {
-
+	sort.Sort(sort.Reverse(sort.IntSlice(numbers)))
+	reduced := numbers[:2]
+	return reduced
 }
 
 func main() {
-	testCase1 := []int {7, 3, 7, 4, 2, 1, 5}
-	result := getTwoLargestInteger(testCase1)
+	testCase := []int {7, 3, 7, 4, 2, 1, 5}
+	result := getTwoLargestInteger(testCase)
+	fmt.Println(result)
+
+	testCase = []int {7, 3, 9, 4, 2, 1, 5}
+	result = getTwoLargestInteger(testCase)
+	fmt.Println(result)
+	
+	testCase = []int {1, 2, 3, 4, 5}
+	result = getTwoLargestInteger(testCase)
+	fmt.Println(result)
+	
+	testCase = []int {1, 3, 5, 7, 9, 2, 4, 6, 8, 0}
+	result = getTwoLargestInteger(testCase)
+	fmt.Println(result)
+	
+	testCase = []int {100, 101, 100, 100, 101}
+	result = getTwoLargestInteger(testCase)
+	fmt.Println(result)
+	
+	testCase = []int {1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
+	result = getTwoLargestInteger(testCase)
+	fmt.Println(result)
+	
+	testCase = []int {2, 3}
+	result = getTwoLargestInteger(testCase)
+	fmt.Println(result)
+	
+	testCase = []int {42, 13, 57, 81, 29, 66, 38, 95, 72, 20, 89, 10, 47, 63, 55, 12, 86, 51, 77,
+		32}
+	result = getTwoLargestInteger(testCase)
+	fmt.Println(result)
+	
+	testCase = []int {15, 61, 25, 37, 52, 10, 47, 32, 73, 41, 88, 12, 64, 56, 29, 83, 98, 70, 90,
+		21}
+	result = getTwoLargestInteger(testCase)
 	fmt.Println(result)
 }
