c82c04950e097413b38c0759cc818ddbfec95c25
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-03-24 13:46:26 +0100

    Add solution for go question 4

--- question-4-go/main.go
+++ question-4-go/main.go
@@ -12,9 +12,31 @@ type Transaction struct {
 	Quantity  int // quantity can be positive or negative
 }
 
-func updateInventory(s *Store, t Transaction) {
+func updateInventory(s *Store, t Transaction) bool {
+	itemQuantity, itemExists := s.Inventory[t.ItemName]
 
+	// Add Item
+	if t.Quantity > 0 {
+		if !itemExists {
+			s.Inventory[t.ItemName] = t.Quantity
+		} else {
+			s.Inventory[t.ItemName] += t.Quantity
+		}
+		return true
+	}
+
+	// Delete Item
+	if t.Quantity < 0 {
+		if !itemExists || itemQuantity + t.Quantity < 0 {
+			return false
+		}
+		s.Inventory[t.ItemName] += t.Quantity
+		return true
+	}
+
+	return false
 }
+
 
 func main() {
 	s := &Store{
@@ -27,8 +49,113 @@ func main() {
 		ItemName: "Item 1",
 		Quantity: -5,
 	}
-	updateInventory(s, t)
-	fmt.Println("Updated store:")
-	fmt.Println(*s)
+	result := updateInventory(s, t)
+	fmt.Println(result)
+	if result {
+		fmt.Println("Store after execution:")
+		fmt.Println(*s)
+	} 
 
+	s= &Store{
+		Name: "Local Store",
+		Inventory: map[string]int{
+					 "Apples":  100,
+					 "Bananas": 50,
+					 "Oranges": 30,
+		},
+	}
+	t = Transaction{
+			ItemName: "Bananas",
+			Quantity: -60,
+	}
+	result = updateInventory(s, t)
+	fmt.Println(result)
+	if result {
+		fmt.Println("Store after execution:")
+		fmt.Println(*s)
+	} 
+
+	s = &Store{
+		Name: "Store 2",
+		Inventory: map[string]int{
+					 "Item 1": 15,
+					 "Item 2": 25,
+	}, }
+	t = Transaction{
+			ItemName: "Item 3",
+			Quantity: 10,
+	}
+	result = updateInventory(s, t)
+	fmt.Println(result)
+	if result {
+		fmt.Println("Store after execution:")
+		fmt.Println(*s)
+	} 
+
+	s = &Store{
+		Name: "Store 3",
+		Inventory: map[string]int{
+					"Item 1": 10,
+					"Item 2": 5,
+		},
+	}
+	t = Transaction{
+		ItemName: "Item 2",
+		Quantity: -10,
+	}
+	result = updateInventory(s, t)
+	fmt.Println(result)
+	if result {
+		fmt.Println("Store after execution:")
+		fmt.Println(*s)
+	} 
+
+	s = &Store{
+		Name: "Store 4",
+		Inventory: map[string]int{
+					 "Item 1": 5,
+	}, }
+	t = Transaction{
+			ItemName: "Item 1",
+			Quantity: -10,
+	}
+	result = updateInventory(s, t)
+	fmt.Println(result)
+	if result {
+		fmt.Println("Store after execution:")
+		fmt.Println(*s)
+	} 
+
+	s = &Store{
+		Name: "Store 5",
+		Inventory: map[string]int{
+					 "Item 1": 10,
+					 "Item 2": 5,
+		},
+	}
+	t = Transaction{
+			ItemName: "Item 2",
+			Quantity: 20,
+	}
+	result = updateInventory(s, t)
+	fmt.Println(result)
+	if result {
+		fmt.Println("Store after execution:")
+		fmt.Println(*s)
+	} 
+
+	s = &Store{
+		Name: "Store 6",
+		Inventory: map[string]int{},
+	}
+	t = Transaction{
+				ItemName: "Item 1",
+				Quantity: 5,
+	}
+	result = updateInventory(s, t)
+	fmt.Println(result)
+	if result {
+		fmt.Println("Store after execution:")
+		fmt.Println(*s)
+	} 
 }
