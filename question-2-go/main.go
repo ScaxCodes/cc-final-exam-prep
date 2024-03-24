c0120527785527b24e6b0fed1704bfc771110bd3
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-03-24 12:25:29 +0100

    Add solution for go question 2

--- question-2-go/main.go
+++ question-2-go/main.go
@@ -6,18 +6,18 @@ func oddSumMaxPair(numbers []int) []int {
 	maxPair := make([]int, 2)
 	maxPairSum := 0
 
-	for _, value := range numbers {
-		for i := value; i < len(numbers); i++ {
+	for index, value := range numbers {
+		for i := index; i < len(numbers); i++ {
 			currentSum := value + numbers[i]
-			if currentSum % 2 != 0 && currentSum > maxPairSum {
-				fmt.Printf("%d + %d is odd and has a higher sum!\n", value, numbers[i])
-				maxPair[0] = value
-				maxPair[1] = numbers[i]
-				maxPairSum = maxPair[0] + maxPair[1]
+			if currentSum % 2 != 0 && (currentSum > maxPairSum || maxPairSum == 0) {
+					// fmt.Printf("%d + %d is odd and has a higher sum!\n", value, numbers[i])
+					maxPair[0] = value
+					maxPair[1] = numbers[i]
+					maxPairSum = maxPair[0] + maxPair[1]
 			}
 		}
 	}
-
+	if maxPair[0] == 0 && maxPair[1] == 0 { return nil }
 	return maxPair
 }
 
@@ -55,18 +55,3 @@ func main() {
 	fmt.Println(result)
 	
 }
-
-/* PSEUDO CODE
-loop first element
-	1 + 2, if even return
-		if odd && > largestSum -> store both element in slice
-	1 + 3, if even return
-		(...)
-	(...)
-	stop when slice.length reached
-loop second element
-	2 + 3
-	2 + 4
-	(...) like above
-return largestSum
-*/
