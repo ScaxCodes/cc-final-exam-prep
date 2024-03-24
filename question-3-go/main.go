30f62fc157038314d3191700e632ba4a423a5699
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-03-24 13:00:28 +0100

    Add solution for go question 3

--- question-3-go/main.go
+++ question-3-go/main.go
@@ -9,10 +9,21 @@ type Book struct {
 }
 
 func topAuthors(books []Book) map[string]int {
-	return map[string]int{
-		"Author 1": 13000,
-		"Author 2": 10000,
+	authorSales := make(map[string]int)
+
+	for _, book := range books {
+    _, ok := authorSales[book.Author]
+    if !ok {
+        authorSales[book.Author] = 0
+    }
+    authorSales[book.Author] += book.Sales
 	}
+
+	for author, sales := range authorSales {
+		if sales < 10000 { delete(authorSales, author) }
+	}
+
+	return authorSales
 }
 
 func main() {
@@ -43,7 +54,120 @@ func main() {
         Sales:  2000,
     },
 	}
-
 	result := topAuthors(books)
+	fmt.Println(result)
+
+	books = []Book{
+    {
+        Title:  "The Great Escape",
+        Author: "John Doe",
+        Sales:  4500,
+    },
+    {
+        Title:  "The Lost World",
+        Author: "Michael Crichton",
+        Sales:  20000,
+    },
+    {
+        Title:  "The Final Countdown",
+        Author: "John Doe",
+        Sales:  8000,
+    },
+    {
+        Title:  "The Tale of Two Cities",
+        Author: "Charles Dickens",
+        Sales:  5000,
+    },
+    {
+        Title:  "Under the Dome",
+        Author: "Stephen King",
+        Sales:  25000,
+    },
+    {
+        Title:  "Pet Sematary",
+        Author: "Stephen King",
+        Sales:  8000,
+    },
+	}
+	result = topAuthors(books)
+	fmt.Println(result)
+
+	books = []Book{
+    {
+        Title:  "Book 1",
+        Author: "Author 1",
+        Sales:  8000,
+    },
+    {
+        Title:  "Book 2",
+        Author: "Author 2",
+        Sales:  6000,
+    },
+    {
+        Title:  "Book 3",
+        Author: "Author 3",
+        Sales:  12000,
+    },
+    {
+        Title:  "Book 4",
+        Author: "Author 1",
+        Sales:  5000,
+    },
+    {
+        Title:  "Book 5",
+        Author: "Author 3",
+        Sales:  1000,
+    },
+    {
+        Title:  "Book 6",
+        Author: "Author 4",
+        Sales:  15000,
+    },
+	}
+	result = topAuthors(books)
+	fmt.Println(result)
+
+	books = []Book{
+    {
+        Title:  "Book 1",
+        Author: "Author 1",
+        Sales:  3000,
+    },
+    {
+        Title:  "Book 2",
+        Author: "Author 3",
+        Sales:  4000,
+    },
+    {
+        Title:  "Book 3",
+        Author: "Author 1",
+        Sales:  2000,
+    },
+    {
+        Title:  "Book 4",
+        Author: "Author 3",
+        Sales:  7000,
+    },
+	}
+	result = topAuthors(books)
+	fmt.Println(result)
+
+	books = []Book{
+    {
+        Title:  "Book 1",
+        Author: "Author 1",
+        Sales:  5000,
+    },
+    {
+        Title:  "Book 2",
+        Author: "Author 2",
+        Sales:  2000,
+    },
+	}	
+	result = topAuthors(books)
+	fmt.Println(result)
+
+	books = []Book{}
+	result = topAuthors(books)
 	fmt.Println(result)
 }
